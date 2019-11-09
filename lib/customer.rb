c19774b60bc5d176912201e48a9d6391b66a7704
Jeff Swisher <jefftswisher@gmail.com>
2019-11-09 17:44:46 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,16 @@
 class Customer
   attr_accessor :name, :age 
   
+  @@all = []
+  
   def initialize(name, age)
     @age = age 
     @name = name 
+    @@all << self 
   end 
   
+  def self.all 
+    @@all 
+  end 
   
 end
